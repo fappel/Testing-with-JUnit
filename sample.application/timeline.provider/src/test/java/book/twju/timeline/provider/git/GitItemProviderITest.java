03966ce87e9aa794f8abe947bf77634b5596e39f
Frank Appel <fappel@codeaffine.com>
2015-07-06 09:49:45 +0200

    Try to fix build problem

--- sample.application/timeline.provider/src/test/java/book/twju/timeline/provider/git/GitItemProviderITest.java
+++ sample.application/timeline.provider/src/test/java/book/twju/timeline/provider/git/GitItemProviderITest.java
@@ -17,6 +17,9 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
+import book.twju.timeline.test.util.GitRepository;
+import book.twju.timeline.test.util.GitRule;
+
 public class GitItemProviderITest {
   
   private static final String CLONE_NAME = "test";
